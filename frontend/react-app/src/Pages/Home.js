75cb5aa51ddb3e23862461f7703bd68a118b0000
biradarvivek <biradarvivek03@gmail.com>
2024-12-30 11:26:03 +0530

    casedashboard

--- frontend/react-app/src/Pages/Home.js
+++ frontend/react-app/src/Pages/Home.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { LayoutDashboard, Files } from 'lucide-react';
+import React, { useState } from "react";
+import { LayoutDashboard, Files } from "lucide-react";
 import ReportGenerator from "../components/MainDashboardComponents/GenerateReport";
 import { cn } from "../lib/utils";
 import { ScrollArea } from "../components/ui/scroll-area";
-import Sidebar from '../components/Sidebar';
-import MainDashboard from '../components/MainDashboardComponents/MainDashboard';
+import Sidebar from "../components/Sidebar";
+import MainDashboard from "../components/MainDashboardComponents/MainDashboard";
+import { Toaster } from "../components/ui/toaster";
 
 const Dashboard = () => {
-  const [activeTab, setActiveTab] = useState('Dashboard');
-  
+  const [activeTab, setActiveTab] = useState("Dashboard");
 
   // const navItems = [
   //   // { name: 'Dashboard', icon: LayoutDashboard, id: 'Dashboard' },
@@ -18,52 +18,56 @@ const Dashboard = () => {
   // ];
 
   const navItems = [
-      {
-        title: "Dashboard",
-        url: "#",
-        icon: LayoutDashboard,
-        isActive: true,
-      },
-      {
-        title: "Generate Report",
-        url: "#",
-        icon: Files,
-      },
-      {
-        title: "Analytics",
-        url: "#",
-        icon: Files,
-      },
-      {
-        title: "Import to Tally",
-        url: "#",
-        icon: Files,
-      },
-      {
-        title: "Eligibility",
-        url: "#",
-        icon: Files,
-      },
-    ]
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: LayoutDashboard,
+      isActive: true,
+    },
+    {
+      title: "Generate Report",
+      url: "#",
+      icon: Files,
+    },
+    {
+      title: "Analytics",
+      url: "#",
+      icon: Files,
+    },
+    {
+      title: "Import to Tally",
+      url: "#",
+      icon: Files,
+    },
+    {
+      title: "Eligibility",
+      url: "#",
+      icon: Files,
+    },
+  ];
 
   const handleOnNavigate = (selectedNav) => {
-    console.log('Navigating to:', selectedNav);
+    console.log("Navigating to:", selectedNav);
     setActiveTab(selectedNav);
-  }
+  };
 
   return (
     <>
       <div className={cn("h-full w-full flex h-screen bg-background")}>
-        <Sidebar navItems={navItems} activeTab={activeTab} setActiveTab={setActiveTab}/>
+        <Sidebar
+          navItems={navItems}
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+        />
         <ScrollArea className="w-full">
           <div className="flex-1 flex flex-col overflow-hidden">
             <main className="flex-1">
-              {activeTab === 'Dashboard' && <MainDashboard />} 
-              {activeTab === 'Generate Report' && <ReportGenerator />}
+              {activeTab === "Dashboard" && <MainDashboard />}
+              {activeTab === "Generate Report" && <ReportGenerator />}
             </main>
           </div>
         </ScrollArea>
-
+        <Toaster />
       </div>
     </>
   );
--- frontend/react-app/src/components/CaseDashboardComponents/AccountNumNameManager.js
+++ frontend/react-app/src/components/CaseDashboardComponents/AccountNumNameManager.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const AccountNumNameManager = () => {
-  return (
-    <div>AccountNumNameManager</div>
-  )
-}
+  return <div>AccountNumNameManager</div>;
+};
 
-export default AccountNumNameManager
+export default AccountNumNameManager;
